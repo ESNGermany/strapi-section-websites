8313c239c52c7d9c7657f842001dd77350e0f194
ESN Germany Webmaster <webmaster@esn-germany.de>
2021-06-17 14:47:56 +0200

    Inital commit with base from ESN Freiburgs website

---
+++ config/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  host: env('HOST'),
+  port: env.int('PORT'),
+  url:  env('URL'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '15196318-567c-44ed-af1c-4d8c74cb46df'
+};
