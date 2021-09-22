dee1caae84a138dd5fd5c111b191c0a0630385c3
kciy <immernochnichtwach@hab-verschlafen.de>
2021-09-22 16:30:30 +0200

    add websiteMetaDescription

--- config/database.js
+++ config/database.js
@@ -4,15 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      options: {}
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
+
